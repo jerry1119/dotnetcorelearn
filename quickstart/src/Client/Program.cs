96607312f444af5efc5c4a3362f8aedf1d9cff23
杰 <hejie331493066@live.com>
2020-09-07 06:37:25 +0800

    Client Credentials方式授权

---
+++ quickstart/src/Client/Program.cs
@@ -0,0 +1,51 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //创建http客户端
+            //IdentityModel这个类库，给httpclient加了扩展方法，实现identity的相关操作
+            var client = new HttpClient();
+            var disco  = await client.GetDiscoveryDocumentAsync("https://localhost:50012");
+            if (disco.IsError)
+            {
+                System.Console.WriteLine(disco.Error);
+                return;
+            }
+            //请求identityServer来获取token 这里采用的是credential的方式类似于账号密码
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest{
+                Address = disco.TokenEndpoint,
+
+                ClientId = "client",
+                ClientSecret = "secret",
+                Scope = "api1"
+            });
+            if (tokenResponse.IsError)
+            {
+                System.Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+            System.Console.WriteLine(tokenResponse.Json);
+            //发送access token去请求api 通常是使用http授权头，可以通过SetBearerToken扩展方法实现
+            var apiClient = new HttpClient();
+            apiClient.SetBearerToken(tokenResponse.AccessToken);
+            var response = await apiClient.GetAsync("https://localhost:60011/identity");
+            if (!response.IsSuccessStatusCode)
+            {
+                System.Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                System.Console.WriteLine(content);
+            }
+        }
+    }
+}
---
+++ quickstart/src/IdentityServer/Config.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            { 
+                new IdentityResources.OpenId()
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+                new ApiScope("api1","My API")
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[] 
+            { 
+                new Client
+                {
+                    //clientID 和 clientSecrets 这种方式可以理解为登陆账号和密码
+                    ClientId = "client",
+                    //没有交互的用户，通过 clientid/secrit 作为认证方式
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    //认证方式的密码
+                    ClientSecrets = {
+                        new Secret("secret".Sha256())
+                    },
+                    //client 能访问的 scopes
+                    AllowedScopes = {"api1"}
+
+                }
+            };
+    }
+}
