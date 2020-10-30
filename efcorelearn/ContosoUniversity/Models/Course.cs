d8d3b72655222d196625dda083bc24179f8e5ffb
杰 <hejie331493066@live.com>
2020-10-30 23:28:25 +0800

    razor page and efcore 1,Init

---
+++ efcorelearn/ContosoUniversity/Models/Course.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]     //应用可以通过DatabaseGenerated特性指定主键
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
